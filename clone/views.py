e588482b0743b5c5acebf632d1c7a6dbe8fd9ef0
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-10 15:55:27 +0300

    added a register request

--- clone/views.py
+++ clone/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render,redirect
+from .models import Profile
+from django.http import HttpRequest,HttpResponse,HttpResponseRedirect
+from .forms import loginForm,UserRegistratinForm,UserEditForm,ProfileEditForm,ImageForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from  django.contrib import messages
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -10,4 +18,16 @@ def reqister(request):
             new_user = user_form.save(commit= False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            
+            Profile.objects.create(user=new_user)
+            messages.Success(request ,'Account created successfully')
+            return redirect('dashboard')
+
+        else:
+            messages.error(request, 'Error creating your account')
+            return render(request,'account/register.html', {'user_form':user_form})
+
+    else:
+        user_form = UserRegistratinForm()
+        return render(request, 'account/register.html' , {'user_form':user_form})
+
+        

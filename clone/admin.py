3483a0c7972c819824b2b97c19b92bf3fcf5afea
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-11 10:39:19 +0300

    registered imagemodeule

--- clone/admin.py
+++ clone/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Image
 
 # Register your models here.
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','location', 'photo', 'bio')
-
-
 
 admin.site.register(Profile)
+admin.site.register(Image)

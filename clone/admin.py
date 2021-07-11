749bff6e9ee8c64029bccc1492f8710f7b114d61
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-11 09:33:06 +0300

    registerd profile to admin

--- clone/admin.py
+++ clone/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','location', 'photo', 'bio')
+
+
+
+admin.site.register(Profile)

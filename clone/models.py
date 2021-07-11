378f1a6955ce1233e539ef266e39a9643b3ca23f
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-11 10:16:00 +0300

    added image model

--- clone/models.py
+++ clone/models.py
@@ -29,4 +29,51 @@ class  Profile(models.Model):
     def get_profile(cls,id):
         profile = cls.objects.get(id = id)
         return profile
+
+class Image(models.Model):
+    name = models.CharField(max_length=30, blank=True)
+    image = models.ImageField(upload_to='posts', blank=True)
+    caption = models.TextField(blank=True)
+    profile = models.ForeignKey(Profile , on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    created_at =models.DateTimeField(auto_now_add=True,blank=True,null =True)
+
+    class Meta:
+        ordering = ['-pk']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def all_likes(self):
+        return self.likes.count()
+        
+    def __str__(self):
+        return self.name
     
+
+    @classmethod
+    def update_caption(cls,pk,caption_update):
+        image = cls.objects.get(pk = pk)
+        image.update(caption = caption_update)
+
+    @classmethod
+    def get_image(cls,id):
+        image = cls.objects.get(id = id)
+        return image
+
+class Comment(models.Model):
+    commenting = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    post = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+
+

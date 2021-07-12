fd1afc1a1b41c5fc26d3acbc1566035457801576
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-12 08:12:39 +0300

    addded a image filed

--- clone/models.py
+++ clone/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class  Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     location = models.CharField(max_length=50, blank=True)
-    photo = models.ImageField(upload_to='users/' , blank=True , default='../static/photos/profile.jpeg')
+    photo = models.ImageField(upload_to='media/users/' , blank=True , default='../static/photos/profile.jpeg')
     bio = models.TextField(max_length=350, default="Hello their am using instagram_clone", blank=True)
 
     def save_profile(self):
@@ -65,7 +68,7 @@ class Image(models.Model):
         return image
 
 class Comment(models.Model):
-    commenting = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    commenting = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
     comment = models.TextField()
     post = models.ForeignKey(Image, on_delete=models.CASCADE)
 
@@ -77,10 +80,43 @@ class Comment(models.Model):
 
 
 class Follow(models.Model):
-    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
-    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+    user_from = models.ForeignKey(User , related_name = 'rel_from_set' , on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User , related_name ='rel_to_set' ,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.follower} Follow'
-                  
+User.add_to_class('following',
+                  models.ManyToManyField('self',
+                                        through=Follow,
+                                        related_name='followers',
+                                        symmetrical=False
+                                        ))
 
+# class Post(models.Model):
+#     image = models.ImageField(upload_to='posts/')
+#     name = models.CharField(max_length=250, blank=True)
+#     caption = models.CharField(max_length=250, blank=True)
+#     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+#     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+#     created = models.DateTimeField(auto_now_add=True, null=True)
+#     image = CloudinaryField('image')
+
+#     class Meta:
+#         ordering = ["-pk"]
+
+#     def get_absolute_url(self):
+#         return f"/post/{self.id}"
+
+#     @property
+#     def get_all_comments(self):
+#         return self.comments.all()
+
+#     def save_image(self):
+#         self.save()
+
+#     def delete_image(self):
+#         self.delete()
+
+#     def total_likes(self):
+#         return self.likes.count()
+
+#     def __str__(self):
+#         return f'{self.user.name} Post'

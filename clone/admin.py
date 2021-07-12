6cae1bcf3a6f3831bddcb4f820ac04c78678229a
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-12 08:10:36 +0300

    registed follo and comment module

--- clone/admin.py
+++ clone/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Image
+from .models import Profile,Image,Comment,Follow
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Follow)
+admin.site.register(Comment)

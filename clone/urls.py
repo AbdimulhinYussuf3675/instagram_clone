1e722a0637978c263c6d0589b0c472dd98d7a3ee
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-12 08:16:53 +0300

    added a urls

--- clone/urls.py
+++ clone/urls.py
@@ -9,7 +9,14 @@ urlpatterns = [
     url(r'^register/', views.register,name = 'register'),
     url(r'^login/' , auth_views.LoginView.as_view() ,name ='login'),
     url(r'^logout/' , auth_views.LogoutView.as_view(),{"next_page": '/'} ,name ='logout' ),
-
+    url(r'^edit/', views.edit, name='edit'),
+    url(r'^create/', views.create, name='create'),
+    url(r'^profile/', views.profile, name= 'profile'),
+    url(r'^profile/<username>', views.get_profile, name= 'get_profile'),
+    url(r'^comment/<int:pk>', views.comment , name= 'comment'),
+    url(r'^follow/<int:user_to>', views.follow , name= 'follow'),
+    url(r'^like/', views.like , name= 'like'),
+    url(r'^searches/' , views.searches , name = 'searches')
    
 ]
 
--- clone/views.py
+++ clone/views.py
@@ -8,25 +8,24 @@ from  django.contrib import messages
 from django.contrib.auth.models import User
 
 
-# Create your views here.
 
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistratinForm(request.POST)
         
         if user_form.is_valid():
-            # create a new user object but avoid saving it yet
+           
             new_user = user_form.save(commit= False)
-            # set the chose password
+           
             new_user.set_password(user_form.cleaned_data['password'])
-            # save the new user
+            
             new_user.save()
             Profile.objects.create(user= new_user)
-            messages.success(request ,'Account created successfully you can login')
+            messages.success(request ,'Account created successfully')
             return redirect('dashboard')
 
         else:
-            messages.error(request ,'This account already exist')
+            messages.error(request ,'Error creating your account')
             return render(request,'account/register.html' , {'user_form':user_form})
 
 
@@ -37,31 +36,146 @@ def register(request):
 
 
 
-@login_required(login_url='login')
+@login_required
 def dashboard(request):
     posts = Image.objects.all()
     comments = Comment.objects.all()
     
 
-    return render(request,'account/dashboard.html', {'posts':posts , 'comments':comments})      
+    return render(request,'account/dashboard.html', {'posts':posts , 'comments':comments})
 
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.profile, data=request.POST, files=request.FILES)
+        user_form =UserEditForm(instance=request.user , data=request.POST)
+        profile_form =ProfileEditForm(instance = request.user.profile, data =request.POST, files=request.FILES)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request , 'Your profile is updated as successfully')
-
+            messages.success(request ,'Profie updated successfully')
+        
         else:
-            messages.error(request,'Theie is an error while updating your profile')
+            messages.error(request,'Error updating profile')
 
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form=ProfileEditForm(instance=request.user.profile)
 
-    return render(request, 'account/edit.html', {'user_form': user_form , 'profile_form':profile_form})
+    return render(request, 'account/edit.html', {'user_form':user_form , 'profile_form':profile_form})
 
+@login_required
+def create(request):
+    if request.method == 'POST':
+        image_form = ImageForm(request.POST,request.FILES)
+        profile = Profile.objects.get(user__id = request.user.id)
+
+        if image_form.is_valid():
+            post = image_form.save(commit = False)
+            post.profile = profile
+            post.save()
+
+            messages.success(request ,'New Post added successfully')
+            return redirect('dashboard')
+
+    else:
+        image_form= ImageForm()
+        return render(request,'account/post.html', {'image_form':image_form})
+
+def profile(request):
+    profile = Profile.objects.get(user__id = request.user.id)
+    images = Image.objects.filter(profile = profile)
+
+    followers = Follow.objects.filter(user_to = request.user).count()
+    following = Follow.objects.filter(user_from = request.user).count()
+    return render(request ,'account/profile.html' , {'profile':profile , 'images':images , "followers":followers,"following":following})
+
+
+def get_profile(request,username):
+  
+    profile = Profile.objects.get(user__username = username) 
+    images = Image.objects.filter(profile = profile)
+    user = User.objects.get(username=username)
+
+    if Follow.objects.filter(user_from=request.user,user_to = user).exists():
+        is_follow =True
+    else:
+        is_follow =False
+    
+    followers = Follow.objects.filter(user_to = user).count()
+    following = Follow.objects.filter(user_from = user).count()
+
+    return render(request ,'account/profile.html' , {'profile':profile , 'images':images , "is_follow":is_follow,"followers":followers,"following":following})
+
+
+def comment(request,pk):
+    post = Image.objects.get(pk =pk)
+
+    if request.POST.get('comment'):
+        new_comment = Comment(comment =request.POST.get('comment'))
+        new_comment.commentor = request.user
+        new_comment.post = post
+        new_comment.save()
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    else:
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def follow(request,user_to):
+
+    user = User.objects.get(id=user_to)
+    is_follow = False
+    rel = Follow.objects.filter(user_from = request.user , user_to = user)
+    if rel.exists():
+        rel.delete()
+        is_follow = False
+    else:
+        Follow(user_from = request.user, user_to = user).save()
+        is_follow =True
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    
+
+def like(request):
+    
+    data = json.loads(request.body)
+    post_id = data['postId']
+    action = data['action']
+
+    post = Image.objects.get(id =post_id)
+    data = {}
+
+    if post.likes.filter(id = request.user.id).exists():
+        post.likes.remove(request.user)
+        data = {'is_liked':False}
+          
+        
+    else:
+        post.likes.add(request.user)
+        data = {'is_liked':True}
+
+    print(post_id)
+    print(action)
+    
+    return JsonResponse(data)
+
+def searches(request):
+    if request.GET.get('search'):
+        search = request.GET['search']
+        profiles = Profile.objects.filter(user__username__icontains = search)
+        searchterm = f'{search}'
+        return render(request,'account/searches.html' , {'profiles':profiles, 'searchterm':searchterm})
+    else:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    
+
+
+
+
+    
+
+
+    
